40066dcb6bdbb69e3fa440f692892343c6d930d3
Udhayakumar V <93321639+udhayav@users.noreply.github.com>
2023-03-04 18:09:46 +0530

    Add files via upload

---
+++ demo8/script.js
@@ -0,0 +1,22 @@
+
+var userInput = document.getElementById("user_input");
+console.log(userInput);
+var expression = "";
+
+function enter(num){
+  expression += num;
+  userInput.value = expression;
+}
+function equal(){
+  userInput.value = eval(expression); 
+ expression="";  
+}
+function clear(){
+  expression = "";
+  userInput.value = expression;
+}
+function erase(){
+  var length = expression.length;
+  expression = expression.slice(0,length -1);
+  userInput.value = expression;
+}
